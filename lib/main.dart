9ef49db98557716b3e494bd7f579367317b432a7
mortadha1144 <mortadha114@gmail.com>
2022-10-22 12:54:53 +0300

    stack added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:layouts_in_flouter/card_and_stack.dart';
 //import 'package:layouts_in_flouter/container.dart';
-import 'package:layouts_in_flouter/grid_and_list.dart';
+//import 'package:layouts_in_flouter/grid_and_list.dart';
 
 void main() {
-  
-  runApp(const MyGridView());
+  runApp(const MyStack());
 }
