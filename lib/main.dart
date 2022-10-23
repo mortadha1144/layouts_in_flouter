2c7be824b0fb4e6d715400f7946a555787b477d4
mortadha1144 <mortadha114@gmail.com>
2022-10-23 22:56:47 +0300

    tutorial_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:layouts_in_flouter/card_and_stack.dart';
+//import 'package:layouts_in_flouter/card_and_stack.dart';
+import 'package:layouts_in_flouter/tutorial_app.dart';
 //import 'package:layouts_in_flouter/container.dart';
 //import 'package:layouts_in_flouter/grid_and_list.dart';
 
 void main() {
-  runApp(const MyStack());
+  runApp(const MyTutorial());
 }
